be85a5c6a77587f92a91d89831b9a4aecda6f382
Phil <pflavin@gmail.com>
2015-08-07 16:13:51 +0100

    Basic mogoDB hooked up. Simple sitter template. Deployed at baysitter.mongo.com

--- baby_1.js
+++ baby_1.js
@@ -1,10 +1,31 @@
+Sitters = new Mongo.Collection("sitters");
+ 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    sitters: [
-      { name: "Jo" , rating:"xxxx"},
-      { name: "Sam", rating:"xxxxx" },
-      { name: "Katy", rating:"xx" }
-    ]
+    sitters: function () {
+      return Sitters.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-sitter": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var name = event.target.name.value;
+ 
+      // Insert a task into the collection
+      Sitters.insert({
+        name: name,
+        rating: "***",
+        location: "Singapore",
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.name.value = "";
+    }
   });
 }
